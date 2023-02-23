575042f27fdfb4638b71fdf346a63af375629490
MarlenZeyn <zmarlen45@gmail.com>
2023-02-23 14:17:00 +0600

    gateway

---
+++ src/test/java/kz/daracademy/employeediscoveryserver/EmployeeDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.daracademy.employeediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
